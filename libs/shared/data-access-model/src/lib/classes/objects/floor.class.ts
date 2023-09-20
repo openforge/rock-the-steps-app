b952b5f0d9b40c3d1983f2bcdc02c06d490ffb55
emmanuelOpenForge <106994064+emmanuelOpenForge@users.noreply.github.com>
2023-09-20 06:43:48 -0700

    Scaling for steps and floors (#62)
    * Added stairs and floor logic to have same size
    Added new speed for floor and stairs so they will join soon
    
    * fix(): debug to false
    
    ---------
    
    Co-authored-by: Emmanuel Castro <emmanuel.castro.pantoja@gmail.com>
    Co-authored-by: Ricardo Madrid <ricardomadrid385@gmail.com>

--- libs/shared/data-access-model/src/lib/classes/objects/floor.class.ts
+++ libs/shared/data-access-model/src/lib/classes/objects/floor.class.ts
@@ -2,6 +2,7 @@ import { CONFIG } from '@openforge/shared-phaser-singleton';
 import { Scene } from 'phaser';
 
 import { FLOOR_KEY } from '../../constants/game-keys.constants';
+import { FLOOR_SCREEN_TARGET_PERCENTAGE } from '../../constants/game-units.constants';
 
 export class Floor {
     public sprite: Phaser.GameObjects.TileSprite; // * Object sprite
@@ -10,7 +11,8 @@ export class Floor {
         console.log('floor.class.ts', 'constructor()');
         // * have to specify the width to make it full
         this.sprite = scene.add.tileSprite(x, y, 0, 0, FLOOR_KEY);
-        this.sprite.setScale(CONFIG.DEFAULT_WIDTH / this.sprite.width);
+        const targetHeight = CONFIG.DEFAULT_HEIGHT * FLOOR_SCREEN_TARGET_PERCENTAGE;
+        this.sprite.setScale(CONFIG.DEFAULT_WIDTH / this.sprite.width, targetHeight / this.sprite.height);
         this.sprite.setOrigin(0, y);
         // * Set the floor height responsively as 10% less than the game height
         // eslint-disable-next-line no-magic-numbers
