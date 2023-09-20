4714e689705d6d952867a05458c12ca6e65efa5e
Ricardo Madrid <ricardomadrid385@gmail.com>
2023-09-19 18:36:06 -0600

    feat/network-error (#61)
    * feat(): component added, UI implemented and network plugin installed
    
    * feat(): modal logic implemented and network change status functionality added

--- apps/rock-the-steps/src/app/app.component.ts
+++ apps/rock-the-steps/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Network } from '@capacitor/network';
 import { ScreenOrientation } from '@capacitor/screen-orientation';
-import { Platform } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
 import { PhaserSingletonService } from '@openforge/shared-phaser-singleton';
+
+import { InternetConnectionFailComponent } from './network/internet-connection-fail/internet-connection-fail.component';
 
 @Component({
     selector: 'openforge-app-root',
@@ -9,12 +12,13 @@ import { PhaserSingletonService } from '@openforge/shared-phaser-singleton';
     styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnDestroy, OnInit {
-    constructor(public phaserInstance: PhaserSingletonService, public platform: Platform) {}
+    constructor(public phaserInstance: PhaserSingletonService, public platform: Platform, private modalController: ModalController) {}
 
     async ngOnInit(): Promise<void> {
         if (this.platform.is('capacitor')) {
             this.setScreenOrientation();
         }
+        void this.checkInternetConnection();
     }
 
     /**
@@ -23,6 +27,34 @@ export class AppComponent implements OnDestroy, OnInit {
      */
     private setScreenOrientation(): void {
         void ScreenOrientation.lock({ orientation: 'landscape-primary' });
+    }
+
+    /**
+     * * Function to check network status
+     *
+     */
+    private async checkInternetConnection(): Promise<void> {
+        const connectionStatus = await Network.getStatus();
+        if (connectionStatus.connectionType === 'none') {
+            void this.showNetworkModal();
+        }
+        void Network.addListener('networkStatusChange', status => {
+            if (status.connectionType === 'none') {
+                void this.showNetworkModal();
+            }
+        });
+    }
+
+    /**
+     * * Function to display Networt Connection Modal
+     *
+     */
+    private async showNetworkModal(): Promise<void> {
+        const modalCtrl = await this.modalController.create({
+            component: InternetConnectionFailComponent,
+            canDismiss: false,
+        });
+        await modalCtrl.present();
     }
 
     /**
--- apps/rock-the-steps/src/app/app.module.ts
+++ apps/rock-the-steps/src/app/app.module.ts
@@ -7,13 +7,14 @@ import { PhaserSingletonService } from '@openforge/shared-phaser-singleton';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomePageComponent } from './home/home.page';
+import { InternetConnectionFailComponent } from './network/internet-connection-fail/internet-connection-fail.component';
 import { PlayStageComponent } from './play-stage/play-stage.component';
 import { ResultScreenComponent } from './result-screen/result-screen.component';
 import { DifficultSelectModalComponent } from './stage-select/difficult-select-modal/difficult-select-modal.component';
 import { StageSelectComponent } from './stage-select/stage-select.component';
 
 @NgModule({
-    declarations: [AppComponent, HomePageComponent, StageSelectComponent, PlayStageComponent, DifficultSelectModalComponent, ResultScreenComponent],
+    declarations: [AppComponent, HomePageComponent, StageSelectComponent, PlayStageComponent, DifficultSelectModalComponent, ResultScreenComponent, InternetConnectionFailComponent],
     imports: [BrowserModule, IonicModule.forRoot(), PhaserSingletonService.forRoot(), AppRoutingModule],
     providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
     bootstrap: [AppComponent],
