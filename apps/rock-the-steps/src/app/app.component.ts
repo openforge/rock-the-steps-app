39a21e8b26656945efbf73673f5221042d1b21ba
Paulina Gallo <paulina@openforge.io>
2023-09-25 17:55:52 -0400

    fix(): create modal service, update dismiss logic for internet connection

--- apps/rock-the-steps/src/app/app.component.ts
+++ apps/rock-the-steps/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { ModalController, Platform } from '@ionic/angular';
 import { PhaserSingletonService } from '@openforge/shared-phaser-singleton';
 
 import { InternetConnectionFailComponent } from './network/internet-connection-fail/internet-connection-fail.component';
+import { ModalService } from './services/modal.service';
 
 @Component({
     selector: 'openforge-app-root',
@@ -12,13 +13,28 @@ import { InternetConnectionFailComponent } from './network/internet-connection-f
     styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnDestroy, OnInit {
-    constructor(public phaserInstance: PhaserSingletonService, public platform: Platform, private modalController: ModalController) {}
+    modalCtrl;
+    constructor(public phaserInstance: PhaserSingletonService, public platform: Platform, private modalController: ModalController, private modalService: ModalService) {}
 
     async ngOnInit(): Promise<void> {
         if (this.platform.is('capacitor')) {
             this.setScreenOrientation();
         }
-        void this.checkInternetConnection();
+
+        const connectionStatus = await Network.getStatus();
+        if (connectionStatus.connectionType === 'none') {
+            void this.showNetworkModal();
+        }
+
+        void Network.addListener('networkStatusChange', status => {
+            if (status.connectionType === 'none') {
+                void this.showNetworkModal();
+            }
+
+            if (status.connectionType !== 'none') {
+                void this.modalService.dismiss();
+            }
+        });
     }
 
     /**
@@ -30,31 +46,14 @@ export class AppComponent implements OnDestroy, OnInit {
     }
 
     /**
-     * * Function to check network status
-     *
-     */
-    private async checkInternetConnection(): Promise<void> {
-        const connectionStatus = await Network.getStatus();
-        if (connectionStatus.connectionType === 'none') {
-            void this.showNetworkModal();
-        }
-        void Network.addListener('networkStatusChange', status => {
-            if (status.connectionType === 'none') {
-                void this.showNetworkModal();
-            }
-        });
-    }
-
-    /**
      * * Function to display Networt Connection Modal
      *
      */
     private async showNetworkModal(): Promise<void> {
-        const modalCtrl = await this.modalController.create({
+        await this.modalService.showModal({
             component: InternetConnectionFailComponent,
-            canDismiss: false,
+            backdropDismiss: false,
         });
-        await modalCtrl.present();
     }
 
     /**
---
+++ apps/rock-the-steps/src/app/services/modal.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+// eslint-disable-next-line import/named
+import { ModalController, ModalOptions } from '@ionic/angular';
+
+@Injectable({ providedIn: 'root' })
+export class ModalService {
+    public showingModal = false; //* This flag is used to know if the modal is already displayed
+    private modalElement!: HTMLIonModalElement; // * Used to show modal
+
+    constructor(private modalController: ModalController) {}
+
+    /**
+     * * Shows modal while data is being loaded
+     */
+    public async showModal(parameters: ModalOptions): Promise<void> {
+        if (!this.showingModal) {
+            this.modalElement = await this.modalController.create(parameters);
+            await this.modalElement.present();
+            this.showingModal = true;
+        }
+    }
+
+    /**
+     * * This function dismisses a modal and sets a flag indicating that the modal is no longer showing.
+     */
+    public async dismiss(): Promise<void> {
+        try {
+            await this.modalElement.dismiss();
+            this.showingModal = false;
+        } catch (e) {
+            console.warn('Error with dismissing modal - ', e);
+        }
+    }
+}
