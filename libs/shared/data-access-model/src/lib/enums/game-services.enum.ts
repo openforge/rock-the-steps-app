e9be6ab25734c12cdba93617d0654b36349a1ad1
Ricardo Madrid <ricardomadrid@ram.local>
2023-09-25 21:25:48 -0600

    fix(): fixed android platform issue naming

--- libs/shared/data-access-model/src/lib/enums/game-services.enum.ts
+++ libs/shared/data-access-model/src/lib/enums/game-services.enum.ts
@@ -1,5 +1,5 @@
 export enum GameServicesEnum {
     FIRST_ACHIEVEMENT_ID = '1_stage_achievement',
     SECOND_ACHIEVEMENT_ID = '2_stage_achievement',
-    LEADERBOARDS_ID = 'openforge.rockthesteps.leaderboard.io',
+    LEADERBOARDS_ID = 'CgkI_b7OpKUXEAIQBA',
 }
