ae6905ceae85a7349f37162e164abdbe44e8eb32
Ricardo Madrid <ricardomadrid385@gmail.com>
2023-09-26 09:44:39 -0600

    fix/android-app (#71)
    * fix(): fixed android platform issue naming
    
    * fix(): added correct package-lock.json file
    
    ---------
    
    Co-authored-by: Ricardo Madrid <ricardomadrid@ram.local>

--- apps/rock-the-steps/capacitor.config.ts
+++ apps/rock-the-steps/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'angular.ionicphaser.openforge.io',
+    appId: 'com.openforge.rockthesteps',
     appName: 'rock-the-steps',
     webDir: '../../dist/apps/rock-the-steps',
     bundledWebRuntime: false,
