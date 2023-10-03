af352bb94cf8ba377ab84a48a4a583d26a10909b
Ricardo Madrid <ricardomadrid385@gmail.com>
2023-10-03 07:33:06 -0600

    fix(): unnecessary code removed

--- apps/rock-the-steps/src/app/play-stage/play-stage.component.ts
+++ apps/rock-the-steps/src/app/play-stage/play-stage.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { GameEnum, JUMP_KEYBOARD, LEFT_KEYBOARD, RIGHT_KEYBOARD, WORLD_SCENE } from '@openforge/shared/data-access-model';
+import { GameEnum } from '@openforge/shared/data-access-model';
 import { PhaserSingletonService } from '@openforge/shared-phaser-singleton';
-import { WorldScene } from 'libs/shared/phaser-singleton/src/lib/scenes/world.scene';
 
 import { GameEngineSingleton } from '../../../../../libs/shared/data-access-model/src/lib/classes/singletons/game-engine.singleton';
 
@@ -39,60 +38,5 @@ export class PlayStageComponent implements OnInit {
      */
     private async initStageScene(): Promise<void> {
         await PhaserSingletonService.init();
-    }
-
-    /**
-     * * Function to detect when keyboards arrow and space are press
-     *
-     * @param event as the KeyboardEvent
-     * @param action as the action to set
-     */
-    public onKeyboardPress(event: KeyboardEvent, action: boolean) {
-        switch (event.key) {
-            case LEFT_KEYBOARD:
-                this.moveToLeft(action);
-                break;
-            case RIGHT_KEYBOARD:
-                this.moveToRight(action);
-                break;
-            case JUMP_KEYBOARD:
-                this.doJump(action);
-                break;
-            default:
-                break;
-        }
-    }
-
-    /**
-     * * Function to move the character to the left or not
-     *
-     * @param isMoving as boolean
-     */
-    public moveToLeft(isMoving: boolean): void {
-        const worldScene = PhaserSingletonService.activeGame.scene.getScene(WORLD_SCENE) as WorldScene;
-        worldScene.character.isMovingLeft = isMoving;
-    }
-
-    /**
-     * * Function to move the character to the right or not
-     *
-     * @param isMoving as boolean
-     */
-    public moveToRight(isMoving: boolean): void {
-        const worldScene = PhaserSingletonService.activeGame.scene.getScene(WORLD_SCENE) as WorldScene;
-        worldScene.character.isMovingRight = isMoving;
-    }
-
-    /**
-     * * Function to do character jump or not
-     *
-     * @param isJumping as boolean
-     */
-    public doJump(isJumping: boolean): void {
-        const worldScene = PhaserSingletonService.activeGame.scene.getScene(WORLD_SCENE) as WorldScene;
-        worldScene.character.isJumping = isJumping;
-        if (GameEngineSingleton.audioService.activeMusic && isJumping) {
-            GameEngineSingleton.audioService.playJump(worldScene);
-        }
     }
 }
