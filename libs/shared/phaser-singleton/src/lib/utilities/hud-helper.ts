b58deb0aeac4f2d387dab84ed281157e414797a0
emmanuelOpenForge <106994064+emmanuelOpenForge@users.noreply.github.com>
2023-10-13 11:55:48 -0500

    Added back buttons instead of touchzones (#102)
    * Added back buttons instead of touchzones
    -  Added back buttons instead touchzones
    - Added pressed state for the buttons of direction
    
    * Added back buttons instead of touchzones
    -  Added back buttons instead touchzones
    - Added pressed state for the buttons of direction
    
    ---------
    
    Co-authored-by: Emmanuel Castro <emmanuel.castro.pantoja@gmail.com>

--- libs/shared/phaser-singleton/src/lib/utilities/hud-helper.ts
+++ libs/shared/phaser-singleton/src/lib/utilities/hud-helper.ts
@@ -1,10 +1,36 @@
 /* eslint-disable no-magic-numbers */
 import { Preferences } from '@capacitor/preferences';
-import { Character, DOWN_EVENT, GameEngineSingleton, MUSIC_BUTTON, MUTE_BUTTON, PAUSE_BUTTON, PAUSE_SCENE, POINTER_DOWN_EVENT, UP_EVENT } from '@openforge/shared/data-access-model';
+import {
+    BUTTON_JUMP_X,
+    BUTTON_LEFT_X,
+    BUTTON_RIGHT_X,
+    BUTTONS_MOVE_Y,
+    Character,
+    CONTROLS_KEY,
+    CONTROLS_LEFT_KEY,
+    CONTROLS_RIGHT_KEY,
+    DOWN_EVENT,
+    GameEngineSingleton,
+    JUMP_KEY,
+    KEYDOWN_EVENT,
+    KEYUP_EVENT,
+    LEFT_KEY,
+    LEFT_KEYBOARD,
+    MUSIC_BUTTON,
+    MUTE_BUTTON,
+    PAUSE_BUTTON,
+    PAUSE_SCENE,
+    POINTER_DOWN_EVENT,
+    POINTER_UP_EVENT,
+    RIGHT_KEY,
+    RIGHT_KEYBOARD,
+    UP_EVENT,
+} from '@openforge/shared/data-access-model';
 import { Scene } from 'phaser';
 import * as Phaser from 'phaser';
 
 import { CONFIG } from '../config';
+import { WorldScene } from '../scenes/world.scene';
 
 /**
  * * Method used to create the buttons of movement and jump
@@ -39,7 +65,91 @@ export async function createButtons(scene: Scene, spaceBarKey: Phaser.Input.Keyb
     spaceBarKey.on(DOWN_EVENT, () => doJumpMovement(scene, character), scene);
     spaceBarKey.on(UP_EVENT, () => (character.isJumping = false), scene);
 }
-
+/**
+ * * Function to create the touch zones in the screen
+ *
+ * @param scene as WorldScene
+ * @param character
+ * @param spaceBarKey
+ * @param keyboard
+ */
+export function createMovementButtons(scene: WorldScene, character: Character, spaceBarKey: Phaser.Input.Keyboard.Key, keyboard: Phaser.Input.Keyboard.KeyboardPlugin): void {
+    console.log('world.scene.ts', 'buttons');
+    const buttonLeft = scene.add.sprite(BUTTON_LEFT_X, CONFIG.DEFAULT_HEIGHT - BUTTONS_MOVE_Y, CONTROLS_KEY, LEFT_KEY);
+    buttonLeft.setScale(CONFIG.DEFAULT_CONTROL_SCALE);
+    buttonLeft.setInteractive();
+    buttonLeft.setDepth(3);
+    buttonLeft.on(
+        POINTER_DOWN_EVENT,
+        () => {
+            buttonLeft.setTexture(CONTROLS_LEFT_KEY, LEFT_KEY);
+            character.isMovingLeft = true;
+        },
+        scene
+    );
+    buttonLeft.on(
+        POINTER_UP_EVENT,
+        () => {
+            buttonLeft.setTexture(CONTROLS_KEY, LEFT_KEY);
+            character.isMovingLeft = false;
+        },
+        scene
+    );
+    const buttonRight = scene.add.sprite(BUTTON_RIGHT_X, CONFIG.DEFAULT_HEIGHT - BUTTONS_MOVE_Y, CONTROLS_KEY, RIGHT_KEY);
+    buttonRight.setScale(CONFIG.DEFAULT_CONTROL_SCALE);
+    buttonRight.setInteractive();
+    buttonRight.setDepth(3);
+    buttonRight.on(
+        POINTER_DOWN_EVENT,
+        () => {
+            buttonRight.setTexture(CONTROLS_RIGHT_KEY, RIGHT_KEY);
+            character.isMovingRight = true;
+        },
+        scene
+    );
+    buttonRight.on(
+        POINTER_UP_EVENT,
+        () => {
+            buttonRight.setTexture(CONTROLS_KEY, RIGHT_KEY);
+            character.isMovingRight = false;
+        },
+        scene
+    );
+    keyboard.on(
+        KEYDOWN_EVENT,
+        (ev: KeyboardEvent) => {
+            if (ev.key === RIGHT_KEYBOARD) {
+                buttonRight.setTexture(CONTROLS_RIGHT_KEY, RIGHT_KEY);
+                character.isMovingRight = true;
+            } else if (ev.key === LEFT_KEYBOARD) {
+                buttonLeft.setTexture(CONTROLS_LEFT_KEY, LEFT_KEY);
+                character.isMovingLeft = true;
+            }
+        },
+        scene
+    );
+    keyboard.on(
+        KEYUP_EVENT,
+        (ev: KeyboardEvent) => {
+            if (ev.key === RIGHT_KEYBOARD) {
+                buttonRight.setTexture(CONTROLS_KEY, RIGHT_KEY);
+                character.isMovingRight = false;
+            } else if (ev.key === LEFT_KEYBOARD) {
+                buttonLeft.setTexture(CONTROLS_KEY, LEFT_KEY);
+                character.isMovingLeft = false;
+            }
+        },
+        scene
+    );
+    const buttonJump = scene.add.sprite(CONFIG.DEFAULT_WIDTH - BUTTON_JUMP_X, CONFIG.DEFAULT_HEIGHT - BUTTONS_MOVE_Y, JUMP_KEY);
+    buttonJump.setScale(CONFIG.DEFAULT_CONTROL_SCALE);
+    buttonJump.setInteractive();
+    buttonJump.setDepth(3);
+    buttonJump.on(POINTER_DOWN_EVENT, () => doJumpMovement(scene, character), scene);
+    buttonJump.on(POINTER_UP_EVENT, () => (character.isJumping = false), scene);
+    spaceBarKey.on(DOWN_EVENT, () => (character.isJumping = true), scene);
+    spaceBarKey.on(UP_EVENT, () => (character.isJumping = false), scene);
+}
 export async function doJumpMovement(scene: Scene, character: Character): Promise<void> {
     const audioPreference = (await Preferences.get({ key: 'EFFECTS_ON' })).value;
     if (scene) {
