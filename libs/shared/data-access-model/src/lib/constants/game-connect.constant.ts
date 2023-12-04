4d926b274d21e62f41fa3783cc6a5fad4ca799d8
Ricardo Madrid <ricardomadrid385@gmail.com>
2023-12-04 14:02:34 -0600

    fix(): google play leaderboard id updated

--- libs/shared/data-access-model/src/lib/constants/game-connect.constant.ts
+++ libs/shared/data-access-model/src/lib/constants/game-connect.constant.ts
@@ -1,3 +1,3 @@
-export const LEADERBOARD_ANDROID_ID = 'CgkI_b7OpKUXEAIQBA';
+export const LEADERBOARD_ANDROID_ID = 'CgkI_b7OpKUXEAIQBg';
 export const LEADERBOARD_IOS_ID = 'openforge.rockthesteps.leaderboard.io';
 export const NOT_DEFINED = 'NOT_DEFINED';
